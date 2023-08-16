5cdc3703141b7bd46a7c992aa99b825a6ec7b8ca
wavepalm <wavepalm@gmail.com>
2023-08-16 11:02:22 +0500

    new

--- routes/report.js
+++ routes/report.js
@@ -4,15 +4,18 @@ const report = require("../schemas/report");
 const router = Router();
 
 router.post("/submit", async (req, res) => {
-  // const { title, description, location } = req.body;
-  console.log(req.body);
   try {
-    await report.create({ title, description, location });
-    res.status(200).send({
-      msg: `report has been registered`,
+    const { title, description, location } = req.body;
+    const newIssue = new report({
+      title,
+      description,
+      location,
     });
+
+    await newIssue.save();
+    res.status(201).json({ message: "Issue added successfully" });
   } catch (error) {
-    res.status(500).json({ error: "Error saving item" });
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 module.exports = router;
