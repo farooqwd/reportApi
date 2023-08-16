0fd687d49bfff0521abb15fe03e92d2bf4504d27
wavepalm <wavepalm@gmail.com>
2023-08-16 16:54:46 +0500

    tweek

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const connectDb = async () => {
   try {
     mongoose.set("strictQuery", false);
     mongoose.connect(
-      "mongodb+srv://farooqwebd:NpRZe0aKIYvicguP@issues.najz3ag.mongodb.net/"
+      "mongodb+srv://farooqwebd:NpRZe0aKIYvicguP@issues.najz3ag.mongodb.net/reports"
     );
     console.log("database connected");
   } catch (error) {
